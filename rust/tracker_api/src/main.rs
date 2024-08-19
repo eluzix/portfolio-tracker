aff7250713091d062608932c73059efc481af73d
Uzi Refaeli <eluzix@gmail.com>
2024-08-19 21:48:15 +0300

    cors + ico

--- rust/tracker_api/src/main.rs
+++ rust/tracker_api/src/main.rs
@@ -1,4 +1,7 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use lambda_http::{
+    http::{response::Builder, Method},
+    run, service_fn, tracing, Body, Error, Request, RequestExt, Response,
+};
 use serde_json::json;
 
 use template_utils::load_tera;
@@ -7,7 +10,29 @@ use tracker_analyzer::helpers::analyze_user_portfolio;
 
 mod template_utils;
 
+fn base_response() -> Builder {
+    Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .header("access-control-allow-origin", "https://eluzix.netlify.app")
+        .header("access-control-allow-methods", "get, post, options")
+        .header(
+            "access-control-allow-headers",
+            "content-type, hx-request, hx-current-url",
+        )
+        .header("access-control-max-age", "7200")
+}
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    if event.method() == Method::OPTIONS {
+        let resp = base_response()
+            // .header("", "")
+            .body(Body::default())
+            .map_err(Box::new)?;
+
+        return Ok(resp);
+    }
+
     if let Some(user_id) = event.query_string_parameters().first("user_id") {
         let portfolio = analyze_user_portfolio(user_id).await.unwrap();
         // let js = serde_json::to_value(&portfolio).unwrap();
@@ -19,10 +44,9 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         ctx.insert("portfolio", &portfolio.portfolio);
 
         let tera = load_tera();
-        let result = tera.render("index.html", &ctx);
-        let resp = Response::builder()
-            .status(200)
-            .header("content-type", "text/html")
+        // let result = tera.render("index.html", &ctx);
+        let result = tera.render("accounts-table.html", &ctx);
+        let resp = base_response()
             // .header("content-type", "application/json")
             // .body(Body::from(serde_json::to_vec(&js).unwrap()))
             .body(Body::Text(result.unwrap()))
