59d17f2b25816fa5ae8d9b57bcf1c765d1899124
Uzi Refaeli <uzi.refaeli@healthee.co>
2024-11-13 16:13:37 +0200

    showing account institution id next to name

--- rust/tracker_console/src/main.rs
+++ rust/tracker_console/src/main.rs
@@ -18,13 +18,13 @@ async fn print_all() {
 
     for (account_id, portfolio_data) in portfolio.accounts.iter() {
         let account_data = portfolio.accounts_metadata.get(account_id).unwrap();
-        println!("--------\nAccount: {}", account_data.name);
-        println!("Portfolio: {:?}", portfolio_data);
+        println!("--------\nAccount: {:?}", account_data);
+        // println!("Portfolio: {:?}", portfolio_data);
     }
 
-    let all_portfolio_data = portfolio.portfolio;
-    println!("--------\nAll Accounts");
-    println!("Portfolio: {:?}", all_portfolio_data);
+    // let all_portfolio_data = portfolio.portfolio;
+    // println!("--------\nAll Accounts");
+    // println!("Portfolio: {:?}", all_portfolio_data);
 }
 
 async fn test_price() {
