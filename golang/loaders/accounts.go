8d4dcc7550856d5886c89fb67ac64f598f92d639
eluzix <eluzix@gmail.com>
2025-09-13 17:38:59 +0300

    load user accounts

---
+++ golang/loaders/accounts.go
@@ -0,0 +1,27 @@
+package loaders
+
+import (
+	"database/sql"
+	"log/slog"
+	"tracker/logging"
+	"tracker/types"
+)
+
+func UserAccounts(db *sql.DB) (*[]types.Account, error) {
+	log := logging.Get()
+	rows, err := db.Query("SELECT id,name,institution from accounts")
+	if err != nil {
+		log.Error("failed to all accounts for user", slog.Any("error", err))
+		return nil, err
+	}
+	defer rows.Close()
+
+	accounts := make([]types.Account, 0)
+	for rows.Next() {
+		var account types.Account
+		_ = rows.Scan(&account.Id, &account.Name, &account.Institution)
+		accounts = append(accounts, account)
+	}
+
+	return &accounts, nil
+}
--- golang/tracker.go
+++ golang/tracker.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"tracker/market"
+	"log/slog"
+	"tracker/loaders"
+	"tracker/logging"
 	"tracker/storage"
 )
 
 func main() {
+	log := logging.Get()
 	db, cleanup := storage.OpenLocalDatabase(false)
 	defer cleanup()
 
-	market.UpdateMarketData(db)
+	// market.UpdateMarketData(db)
 	// log := logging.Get()
 	// log.Info("hello world\n")
+
+	accounts, _ := loaders.UserAccounts(db)
+	log.Info(">>>>>accounts", slog.Any("accounsts", accounts))
 
 }
