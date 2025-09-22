52b4cc23f169f54a1e0a50bfb5178c950d6f5164
eluzix <eluzix@gmail.com>
2025-09-22 09:59:01 +0300

    continue with tui app

--- golang/tui/app.go
+++ golang/tui/app.go
@@ -12,6 +12,59 @@ import (
 	"github.com/rivo/tview"
 )
 
+func AccountsPage(accounts *[]types.Account, accountsData map[string]types.AnalyzedPortfolio, app *tview.Application, pages *tview.Pages) *tview.Table {
+	table := tview.NewTable().SetContent(nil)
+	table.SetBorder(true).SetBorderColor(tcell.ColorGreenYellow)
+	table.SetSelectable(true, false)
+	table.SetSeparator('|').SetBorderPadding(0, 1, 1, 1)
+
+	hs := tcell.StyleDefault.
+		Background(tcell.ColorOrangeRed).
+		Foreground(tcell.ColorGreenYellow).Bold(true)
+
+	table.SetCell(0, 0, tview.NewTableCell("ID").SetStyle(hs))
+	table.SetCell(0, 1, tview.NewTableCell("Account Name").SetStyle(hs))
+	table.SetCell(0, 2, tview.NewTableCell("Total Invested").SetStyle(hs))
+	table.SetCell(0, 3, tview.NewTableCell("Total Withdrawn").SetStyle(hs))
+	table.SetCell(0, 4, tview.NewTableCell("Total Dividends").SetStyle(hs))
+	table.SetCell(0, 5, tview.NewTableCell("Gain").SetStyle(hs))
+	table.SetCell(0, 6, tview.NewTableCell("Annualized Yield").SetStyle(hs))
+	table.SetCell(0, 7, tview.NewTableCell("Dietz Yield").SetStyle(hs))
+	table.SetCell(0, 8, tview.NewTableCell("Value").SetStyle(hs))
+	for i, ac := range *accounts {
+		table.SetCell(i+1, 0, tview.NewTableCell(ac.Id))
+		table.SetCell(i+1, 1, tview.NewTableCell(ac.Name))
+
+		acData, _ := accountsData[ac.Id]
+		table.SetCell(i+1, 2, tview.NewTableCell(utils.ToCurrencyString(acData.TotalInvested)))
+		table.SetCell(i+1, 3, tview.NewTableCell(utils.ToCurrencyString(acData.TotalWithdrawn)))
+		table.SetCell(i+1, 4, tview.NewTableCell(utils.ToCurrencyString(acData.TotalDividends)))
+		table.SetCell(i+1, 5, tview.NewTableCell(utils.ToYieldString(acData.Gain)))
+		table.SetCell(i+1, 6, tview.NewTableCell(utils.ToYieldString(acData.AnnualizedYield)))
+		table.SetCell(i+1, 7, tview.NewTableCell(utils.ToYieldString(acData.ModifiedDietzYield)))
+		table.SetCell(i+1, 8, tview.NewTableCell(utils.ToCurrencyString(acData.Value)))
+	}
+
+	table.SetSelectionChangedFunc(func(row, column int) {
+		if row == 0 {
+			table.Select(1, column)
+		}
+	})
+	table.Select(1, 0).SetFixed(1, 2).SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+		// if key == tcell.KeyEnter {
+		// 	table.SetSelectable(true, true)
+		// }
+	})
+	// .SetSelectedFunc(func(row int, column int) {
+	// 	table.GetCell(row, column).SetTextColor(tcell.ColorRed)
+	// 	table.SetSelectable(false, false)
+	// })
+	return table
+}
+
 func StartApp(db *sql.DB) {
 	accounts, _ := loaders.UserAccounts(db)
 	accountsData := make(map[string]types.AnalyzedPortfolio, len(*accounts))
@@ -26,41 +79,10 @@ func StartApp(db *sql.DB) {
 	wg.Wait()
 
 	app := tview.NewApplication()
-	table := tview.NewTable().SetContent(nil)
-	table.SetBorder(true).SetBorderStyle(tcell.StyleDefault.Background(tcell.ColorRed))
-	table.SetSelectable(true, false)
+	pages := tview.NewPages()
+	pages.AddPage("Accounts", AccountsPage(accounts, accountsData, app, pages), true, true)
 
-	table.SetCell(0, 0, tview.NewTableCell("ID").SetBackgroundColor(tcell.ColorLavender))
-	table.SetCell(0, 1, tview.NewTableCell("Account Name"))
-	table.SetCell(0, 2, tview.NewTableCell("Total Invested"))
-	table.SetCell(0, 3, tview.NewTableCell("Total Withdrawn"))
-	table.SetCell(0, 4, tview.NewTableCell("Total Dividends"))
-	table.SetCell(0, 5, tview.NewTableCell("Value"))
-	for i, ac := range *accounts {
-		table.SetCell(i, 0, tview.NewTableCell(ac.Id))
-		table.SetCell(i, 1, tview.NewTableCell(ac.Name))
-
-		acData, _ := accountsData[ac.Id]
-		table.SetCell(i, 2, tview.NewTableCell(utils.ToCurrencyString(acData.TotalInvested)))
-		table.SetCell(i, 3, tview.NewTableCell(utils.ToCurrencyString(acData.TotalWithdrawn)))
-		table.SetCell(i, 4, tview.NewTableCell(utils.ToCurrencyString(acData.TotalDividends)))
-		table.SetCell(i, 5, tview.NewTableCell(utils.ToCurrencyString(acData.Value)))
-	}
-
-	table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
-		if key == tcell.KeyEscape {
-			app.Stop()
-		}
-		// if key == tcell.KeyEnter {
-		// 	table.SetSelectable(true, true)
-		// }
-	})
-	// .SetSelectedFunc(func(row int, column int) {
-	// 	table.GetCell(row, column).SetTextColor(tcell.ColorRed)
-	// 	table.SetSelectable(false, false)
-	// })
-
-	if err := app.SetRoot(table, true).SetFocus(table).Run(); err != nil {
+	if err := app.SetRoot(pages, true).SetFocus(pages).Run(); err != nil {
 		panic(err)
 	}
 
--- golang/utils/format.go
+++ golang/utils/format.go
@@ -5,3 +5,7 @@ import "fmt"
 func ToCurrencyString(val int64) string {
 	return fmt.Sprintf("$%.2f", float64(val)/100.0)
 }
+
+func ToYieldString(val int32) string {
+	return fmt.Sprintf("%.2f%%", float32(val)/100.0)
+}
