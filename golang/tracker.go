0c389ab23066456e18729cee54a72afd83ede919
eluzix <eluzix@gmail.com>
2025-10-17 14:30:38 +0300

    implement command args

--- golang/tracker.go
+++ golang/tracker.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"tracker/market"
 	"tracker/storage"
 	"tracker/tui"
 )
@@ -9,7 +12,27 @@ func main() {
 	db, cleanup := storage.OpenLocalDatabase(false)
 	defer cleanup()
 
-	// market.UpdateMarketData(db)
+	if len(os.Args) == 2 {
+		switch os.Args[1] {
+		case "help":
+			fmt.Println("Usage: tracker [help|update]")
+			fmt.Println("  help: show this help")
+			fmt.Println("  update: update market data")
+			fmt.Println("  (no args): start the portfolio tracker app")
+		case "update":
+			market.UpdateMarketData(db)
+			fmt.Println("Market data updated successfully")
+		default:
+			fmt.Println("Unknown command")
+			fmt.Println("Usage: tracker [help|update]")
+		}
+
+		return
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		fmt.Println("Usage: tracker [help|update]")
+		return
+	}
 
 	tui.StartApp(db)
 }
